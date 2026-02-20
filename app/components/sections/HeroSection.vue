38b8864813cfe8cc49b021d556079fd62d092a63
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2026-02-20 13:01:14 +0300

    feat: create hero section

--- app/components/sections/HeroSection.vue
+++ app/components/sections/HeroSection.vue
@@ -13,9 +13,11 @@ const items = ref([
 <template>
   <section class="w-full px-[1.6rem] md:px-[3.2rem] h-full pb-[1.6rem]">
     <div
-      class="bg-[--accent] h-full p-[1.6rem] md:p-[2.4rem] rounded-[1.2rem] flex flex-col items-start justify-between"
+      class="bg-[--accent] h-full p-[1.6rem] md:p-[2.4rem] rounded-[1.2rem] flex flex-col items-start justify-between overflow-hidden relative"
     >
-      <div class="flex items-start flex-col md:flex-row gap-[1.2rem]">
+      <div
+        class="flex items-start flex-col md:flex-row gap-[1.2rem] relative z-2"
+      >
         <div
           class="p-[1.6rem] md:p-[2.4rem] bg-[--base] rounded-[0.4rem] w-full text-[--accent] flex flex-col gap-[1.6rem]"
         >
@@ -36,8 +38,14 @@ const items = ref([
           </Bullet>
         </div>
       </div>
-
-      <Button color="base" class="w-full md:w-fit">Записаться на приём</Button>
+      <NuxtImg
+        src="/images/dog.webp"
+        alt="Собака"
+        class="absolute w-[27rem] min-[380px]:w-full min-[390px]:w-[27rem] min-[767px]:w-[60rem] min-[979px]:w-[40rem] min-[1280px]:w-[50rem] min-[1380px]:w-[40rem] h-auto bottom-[-5rem] z-0 md:bottom-[-10rem] left-1/2 -translate-x-1/2"
+      />
+      <Button color="base" class="relative z-10 w-full md:w-fit"
+        >Записаться на приём</Button
+      >
     </div>
   </section>
 </template>
