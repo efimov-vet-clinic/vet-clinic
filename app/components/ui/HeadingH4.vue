62fe39085bd289b5b004dde940f8008c1c01c7c1
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2026-02-26 16:41:43 +0300

    feat: create activity section

--- app/components/ui/HeadingH4.vue
+++ app/components/ui/HeadingH4.vue
@@ -1,6 +1,6 @@
 <template>
   <h4
-    class="text-[3.2rem]/[90%] max-[540px]:text-[2.4rem]/[90%] font-semibold tracking-tight"
+    class="text-[3.2rem]/[90%] max-[650px]:text-[2.4rem]/[90%] font-semibold tracking-tight"
   >
     <slot />
   </h4>
